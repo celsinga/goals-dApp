ee59a3e3ecb2abaec17224056b53b5a015a1c026
Darnell Andries <darnell@andries.ca>
2021-03-24 16:36:14 -0700

    Add web3

--- front-end/my-app/src/App.tsx
+++ front-end/my-app/src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css';
+import Web3 from 'web3';
 
 function App() {
+  const web3 = new Web3(process.env.NODE_ENV === 'production' ? Web3.givenProvider : 'ws://localhost:7545');
+
   return (
     <div className="App">
       
