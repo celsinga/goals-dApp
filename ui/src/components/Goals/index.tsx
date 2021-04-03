98660d425af9ac3787116c0287ddba873e801023
Darnell Andries <darnell@andries.ca>
2021-04-02 23:41:35 -0700

    Ability to save mantra

--- ui/src/components/Goals/index.tsx
+++ ui/src/components/Goals/index.tsx
@@ -2,26 +2,52 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
 import GoalsList from '../GoalsList';
 import styles from './index.css';
-import Goal from '../Goal'
+import Goal from '../Goal';
 import {
   BrowserRouter as Router,
   Switch,
   Route
-} from "react-router-dom";
+} from 'react-router-dom';
+
+const MANTRA_STORAGE_KEY = 'goals_mantra';
 
 export default function Goals() {
-  
+
+  let mantraSaveTimeout: any = null;
+
+  const handleMantraChange = (ev: React.FormEvent<HTMLDivElement>) => {
+    if (!!mantraSaveTimeout) {
+      clearTimeout(mantraSaveTimeout);
+      mantraSaveTimeout = null;
+    }
+    
+    mantraSaveTimeout = setTimeout(() => {
+      window.localStorage.setItem(MANTRA_STORAGE_KEY, (ev.target as HTMLDivElement).innerText);
+    }, 500);
+  }
+
+  let initMantra = window.localStorage.getItem(MANTRA_STORAGE_KEY);
+  if (initMantra === null) {
+    initMantra = `${new Date().getFullYear()} Goals️ ✏️ `;
+  }
 
   return (
     <React.Fragment>
       <div className="homepage">
         <CssBaseline />
         <Container className={styles.goalsMain} maxWidth="md">
-          <Typography className={styles.goalsHeader}>
-            <div contentEditable="true">2021 Goals ✏️</div>
-          </Typography>
+          <Card className={styles.goalsHeader}>
+            <div
+              contentEditable="true"
+              onInput={handleMantraChange}
+              suppressContentEditableWarning
+            >
+              {initMantra}
+            </div>
+          </Card>
           <div className={styles.goalsParent}>
             <Router>
               <Switch>
