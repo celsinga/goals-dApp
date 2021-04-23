631c5d07297451ec80a7393b794a3b92b51e459c
Colin Elsinga <colinelsinga@gmail.com>
2021-04-22 22:25:03 -0700

    update goalsHeavy for edit goal info

--- ui/src/components/Goal/index.tsx
+++ ui/src/components/Goal/index.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import styles from './index.css';
@@ -13,6 +14,8 @@ import { complete } from '../../slices/goals';
 import AddTask from '../AddTask';
 import TaskList from '../TaskList';
 import EditIcon from '@material-ui/icons/Edit';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 
 export default function Goal() {
@@ -32,6 +35,42 @@ export default function Goal() {
     await dispatch(complete(goal!.id));
     history.push('/');
   };
+
+  const defaultDeadlineDate = new Date();
+  defaultDeadlineDate.setHours(0,0,0,0);
+  defaultDeadlineDate.setDate(defaultDeadlineDate.getDate() + 7);
+  defaultDeadlineDate.setMinutes(defaultDeadlineDate.getMinutes() - 
+                                 defaultDeadlineDate.getTimezoneOffset());
+  let defaultDeadline = defaultDeadlineDate.toISOString();
+
+  const [isCreating, setIsCreating] = useState(false);
+
+  const changeDate = function() {
+    return (
+    <div>
+      <TextField
+                type="date"
+                value={deadline}
+                onChange={(ev) => setDeadline(ev.target.value)}
+                label="Deadline"
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
+              <Button
+              type="submit"
+              color='primary'
+              variant='contained'
+              value={isCreating ? 'Creating...' : 'Create'}
+              disabled={ !new Date(deadline).getTime()}
+              >
+               Update Deadline
+          </Button>
+    </div>
+    )
+  }
+
+  const [deadline, setDeadline] = useState(defaultDeadline.substring(0, defaultDeadline.length - 8));
 
   return (
     <div>
@@ -59,7 +98,7 @@ export default function Goal() {
                   {monthsRemaining} {pluralize('month', monthsRemaining)} remaining
                 </Typography>
               )}
-              <EditIcon style={{color: '#eb4034', fontSize: '1.2em', marginLeft: '7px'}} />
+              <EditIcon onClick={changeDate} style={{color: '#eb4034', fontSize: '1.2em', marginLeft: '7px'}} />
             </div>
           </div>
 
